55ce21b0bbd9b1c046ed0e847e332d14ff8fb85e
theuerse <theuers@posteo.de>
2016-03-09 11:12:44 +0100

    added a util-script for recording/archiving changed .log/.json - files in a folder

---
+++ recorder.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SOURCEDIR=$1  # directory to observe for changes of *.log, *.json.
+TARGETDIR=$2  # directory changed files get saved in
+echo "started recording in '$SOURCEDIR', writing to '$TARGETDIR'"
+
+inotifywait -mrq -e close_write --format %w%f $1 | while read FILE
+do
+    filename=$(basename "$FILE")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+
+    if [[ $extension == "json" ]]; then
+      #"JSON-file $FILE was modified, saving..."
+      cp $FILE "$TARGETDIR/stat-PI_${filename#PI}_$(date +"%Y-%m-%d_%H:%M:%S").json"
+    elif [[ $extension == "log" ]]; then
+      #"rtLOG-file $FILE was modified, saving..."
+      cp $FILE $TARGETDIR/$filename.$extension
+    else
+      echo "ignored file $FILE"
+    fi
+done
